dbd35475d78862faf40055dec73e1a748a0f4e64
Patricia Takehara <paty.takeharab@outlook.com>
2022-12-14 16:46:13 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Queue é uma biblioteca (fila)
+//Dequeue remover fila
+
+Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+fila.Enqueue(10);
+
+Console.WriteLine("Adicionando item na fila.");
+
+foreach (int item in fila)
+{
+  Console.WriteLine($"Item  {item}.");
+}
+Console.WriteLine();
+Console.WriteLine($"Removendo item {fila.Dequeue()}.");
+
+foreach (int item in fila)
+{
+  Console.WriteLine($"item {item}.");
+}
+
